9c00806b0e75f7db82e16d7841735ca4e5d7b94f
FilipBorysik <93606629+FilipBorysik@users.noreply.github.com>
2021-11-02 18:47:39 +0100

    Delete Zad_2a.py

--- Zad_2a.py
+++
@@ -1,7 +0,0 @@
-#Lab_2_zad_2a.
-def zad_2a(imiona):
-    for x in imiona:
-        print(x)
-if __name__=='__main__':
-    imiona = ["Jan", "Krzysiek", "Adam", "Marek", "Mariusz"]
-    zad_2a(imiona)
